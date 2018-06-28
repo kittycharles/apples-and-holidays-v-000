063ac301164f13f657ed5c03d07b28bf14756a68
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-28 01:47:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holiday|
       holiday.map do |holiday, item|
-        holiday if item.include?("BBQ")
+        return holiday if item.include?("BBQ")
       end
+
     end
   end
