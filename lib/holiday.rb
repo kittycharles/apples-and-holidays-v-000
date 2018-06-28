df9c6a6aa56fbc6e805466fbbc162203d6ccf73c
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-28 01:54:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,15 +77,4 @@ def all_supplies_in_holidays(holiday_hash)
         holiday if item.include?("BBQ")
       end
     end.flatten.compact
-  
-# def all_holidays_with_bbq(holiday_hash)
-#   # return an array of holiday names (as symbols) where supply lists
-#   # include the string "BBQ"
-#   holiday_hash.map do |season, holidays|
-#     holidays.map do |holiday, supplies|
-#       if supplies.include?("BBQ")
-#         holiday
-#       end
-#     end
-#   end
-end
+  end
