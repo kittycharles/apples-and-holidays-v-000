15ee4690670b66717b1891d09135fe27388abd97
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-28 01:31:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,6 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 holiday_hash.each do |season, holiday|
   holiday << "supply"
 end
+end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
